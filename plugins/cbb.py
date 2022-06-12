f5b4678010e7368965731d641f392cc75a5384b8
Lahiru7 <94838225+Lahiru7@users.noreply.github.com>
2022-06-12 07:27:50 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○  Cʀᴇᴀᴛᴏʀ : <a href='tg://user?id={OWNER_ID}'>Tʜɪs Usᴇʀ</a>\n○ Lᴀɴɢᴜᴀɢᴇ : <code>Pʏᴛʜᴏɴ𝟹</code>\n○ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ ʏᴏᴜᴛᴜʙᴇ : <a href='https://www.youtube.com/channel/UCXQyTKOsTdy3aauIrCpGkhw'>Vɪᴇᴡ ᴄʜᴀɴɴᴇʟ</a>\n○  Cʜᴀɴɴᴇʟ : @MenakaLahiru7\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : @ADL_Drama</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("❗️ 𝑪𝒍𝒐𝒔𝒆 ❗️", callback_data = "close")
                     ]
                 ]
             )
